b438d1c03757d5e3d8acd2290acb811c18c7f94a
George Baronheid <baronheid.george@gmail.com>
2025-02-18 20:45:32 -0300

    chore(): Probes

--- server.go
+++ server.go
@@ -5,13 +5,32 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
+var startedAt = time.Now()
+
 func main() {
+
+	http.HandleFunc("/healthz", Healthz)
 	http.HandleFunc("/secrets", Secrets)
 	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt).Seconds()
+
+	if duration < 10 { // it'll take 10s for my application to start
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Health check failed after %v seconds", duration)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Health check passed after %v seconds", duration)
 }
 
 func Secrets(w http.ResponseWriter, r *http.Request) {
@@ -35,5 +54,5 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 
-	fmt.Fprintf(w, "<h1> Hello, I'm %s and I'm %s years old!!! </h1>", name, age) //w.Write([]byte("<h1> Hello, FullCycle!!! </h1>"))
+	fmt.Fprintf(w, "<h1> Hello, I'm %s and I'm %s years old!!! </h1>", name, age) // w.Write([]byte("<h1> Hello, FullCycle!!! </h1>"))
 }
